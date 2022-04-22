a7b3dd27b9b2444ef3582447398a2f7e1b4365b0
ericbarriga <ericbarriga@MacBook-Pro.local>
2022-04-22 01:03:06 -0600

    added all the componants to my app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Topbar from "./componants/topbar/topbar"
+import Contact from "./componants/contact/contact"
+import Intro from "./componants/intro/intro"
+import Portfolio from "./componants/portfolio/portfolio"
+import Testimonials from "./componants/testimonials/testimonials"
+import Works from "./componants/works/works"
+
+function App() {
+  return (
+    <div className="app">
+      <Topbar />
+      <div className="sections">
+        <Intro />
+        <Portfolio />
+        <Works />
+        <Testimonials />
+        <Contact />
+      </div>
+    </div>
+  );
+}
+
+export default App;
