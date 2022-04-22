1e5128e0a1b1f261c788872ba562bcc42e4d268b
ericbarriga <ericbarriga@MacBook-Pro.local>
2022-04-22 11:46:02 -0600

    working on header info and hamburger

--- src/componants/topbar/topbar.jsx
+++ src/componants/topbar/topbar.jsx
@@ -1,9 +1,31 @@
 import "./topbar.scss";
+import { LocalPhone, Email } from "@mui/icons-material";
+
 
 export default function Topbar() {
     return (
         <div className="topbar">
-
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo" >RETARD.</a>
+                    <div className="itemContainer">
+                        <LocalPhone className="icon" />
+                        <span>303 549 3545</span>
+                    </div>
+                    {/*  */}
+                    <div className="itemContainer">
+                        <Email className="icon" />
+                        <span>eric@gmail.com</span>
+                    </div>
+                </div>
+                <div className="right">
+                    <div className="hamburger">
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
