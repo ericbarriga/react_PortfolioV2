aba4967a143f9403a3769c64e7b07b23bf5023bf
ericbarriga <ericbarriga@MacBook-Pro.local>
2022-04-24 02:05:26 -0600

    added a contact page

--- src/componants/contact/contact.jsx
+++ src/componants/contact/contact.jsx
@@ -1,9 +1,29 @@
 import "./contact.scss";
+import { useState } from "react"
 
 export default function Contact() {
+
+    const [message, setMessage] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setMessage(true)
+    }
+
     return (
         <div className="contact" id="contact">
-            contact
+            <div className="left">
+                <img src="" alt="" />
+            </div>
+            <div className="right">
+                <h2>Contact.</h2>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder="email" />
+                    <textarea name="" id="" cols="30" rows="5" placeholder="message"></textarea>
+                    <button type="submit">send</button>
+                    {message && <span>thank you i will reply ASAP</span>}
+                </form>
+            </div>
         </div>
     )
 }
